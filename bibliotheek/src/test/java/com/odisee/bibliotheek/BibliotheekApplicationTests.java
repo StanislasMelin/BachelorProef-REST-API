a1ba57c833f47117e28ef8892889468060415c01
StanislasMelin <stanislas.melin@student.odisee.be>
2023-05-12 13:57:39 +0200

    initial commit

---
+++ bibliotheek/src/test/java/com/odisee/bibliotheek/BibliotheekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odisee.bibliotheek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotheekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
