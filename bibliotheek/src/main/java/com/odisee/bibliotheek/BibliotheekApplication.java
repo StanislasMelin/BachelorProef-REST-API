c38236bfa7ad62bb187d86ad812f483418a0de2b
StanislasMelin <stanislas.melin@student.odisee.be>
2023-05-15 16:00:38 +0200

    log to indicate which version is running

--- bibliotheek/src/main/java/com/odisee/bibliotheek/BibliotheekApplication.java
+++ bibliotheek/src/main/java/com/odisee/bibliotheek/BibliotheekApplication.java
@@ -1,16 +1,19 @@
 package com.odisee.bibliotheek;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
+@Slf4j
 public class BibliotheekApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(BibliotheekApplication.class, args);
+		log.info("Running non-optimized base REST API.");
 	}
 
 }
